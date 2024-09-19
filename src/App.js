57f69f2e072a788ad7c6a9f81fe2f5c4e429af5c
Oxi - Pi <Hamoon.B.94@gmail.com>
2024-09-19 19:58:59 +0100

    bugfix

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
 import React from 'react';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
 import BingoBoard from './components/BingoBoard';
 
-function App() {
-  return (
-    <div className="App">
-      <BingoBoard />
-    </div>
-  );
-}
+// Custom Theme with OSRS Wiki Colors
+const theme = createTheme({
+  palette: {
+    background: {
+      default: '#c0a886',
+    },
+    body: {
+      main: '#e2dbc8',
+      light: '#d8ccb4',
+      mid: '#d0bd97',
+      dark: '#b8a282',
+      border: '#94866d', // Define the border property here
+      background: '#c0a886',
+    },
+    text: {
+      primary: '#000',
+      secondary: '#777',
+    },
+    divider: '#94866d',
+  },
+});
+
+const App = () => (
+  <ThemeProvider theme={theme}>
+    <CssBaseline />
+    <BingoBoard />
+  </ThemeProvider>
+);
 
 export default App;
