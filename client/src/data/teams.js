146c39e103e62388939a6772954c0770b31bde9e
oxi <hamoon.b.94@gmail.com>
2025-02-06 22:32:15 +0000

    update tile names to finnish and add a team

--- client/src/data/teams.js
+++ client/src/data/teams.js
@@ -2,9 +2,10 @@
 
 const teams = [
     { id: 1, name: "Juipin Jupiterit", color: "#D00000" },
-    { id: 2, name: "Ippin Ipanat", color: "#FFBA08" },
-    { id: 3, name: "Metalin Miehet", color: "#3F88C5" },
-    { id: 4, name: "Ersun Elintarvikkeet", color: "#7FB069" }
+    { id: 2, name: "Ipin Ipanat", color: "#FFBA08" },
+    { id: 3, name: "Metallin Mukulat", color: "#3F88C5" },
+    { id: 4, name: "Ersun Elintarvikkeet", color: "#7FB069" },
+    { id: 4, name: "MiQun minionit", color: "#3EB489" },
   ];
   
   export default teams;
