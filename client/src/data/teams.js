c1a74a709e3376a6879f0e456f0daa1f649a7a30
oxi <hamoon.b.94@gmail.com>
2025-08-22 21:06:09 +0100

    feat: change colours to pastel

--- client/src/data/teams.js
+++ client/src/data/teams.js
@@ -1,11 +1,11 @@
 // src/data/teams.js
 
 const teams = [
-    { id: 1, name: "Ookun Omenat Osk", color: "#D00000" },
-    { id: 2, name: "MiQun Minionit Ky", color: "#FFBA08" },
-    { id: 3, name: "Metallin Miehet Oy", color: "#3F88C5" },
-    { id: 4, name: "Uloksen Ulosotto Tmi", color: "#7FB069" },
-    { id: 5, name: "Ipin Ipanat Ab", color: "#3EB489" },
+    { id: 1, name: "Ookun Omenat Osk", color: "#E88080" },
+    { id: 2, name: "MiQun Minionit Ky", color: "#FFDC83" },
+    { id: 3, name: "Metallin Miehet Oy", color: "#9FC3E2" },
+    { id: 4, name: "Uloksen Ulosotto Tmi", color: "#BFD7B4" },
+    { id: 5, name: "Ipin Ipanat Ab", color: "#9ED9C4" },
   ];
 
   export default teams;
